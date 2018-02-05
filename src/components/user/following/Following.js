2abed8fec9782f30385a803c5afb0a62d1f7897d
Jennifer Elton <justjenny40@hotmail.com>
2018-02-05 12:17:36 -0800

    added routes to user component

--- src/components/user/following/Following.js
+++ src/components/user/following/Following.js
@@ -1,11 +1,11 @@
 import Template from '../Template';
-import html from './upDescription.html';
-import './upDescription.css';
+import html from './following.html';
+import './following.css';
 import { removeChildren } from '../dom';
 
 const template = new Template(html);
 
-export default class UpDescription {
+export default class Following {
 
   render() {
     const dom = template.clone();
