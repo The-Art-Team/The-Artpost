c8ab39c37d6327e2fdb5f512daff17418b00a854
Ivan Limongan <ivan.limongan@gmail.com>
2018-02-05 14:45:05 -0800

    code in place for results, search list, item, image

---
+++ src/components/results/item/Item.js
@@ -0,0 +1,41 @@
+import Template from '../../Template';
+import html from './item.html';
+import './item.css';
+import { db } from '../../../services/firebase';
+import { getUrl } from '../../../services/cloudinary';
+
+const template = new Template(html);
+const itemsImages = db.ref('items-images');
+
+export default class Item {
+  constructor(key, item) {
+    this.key = key;
+    this.item = item;
+    this.itemsImages = itemsImages.child(key).limitToFirst(1);
+  }
+
+  update(item) {
+    this.caption.textContent = `${item.name}`;
+    this.image.alt = item.name;
+  }
+
+  render() {
+    const dom = template.clone();
+
+    dom.getElementById('item-detail') = `#results/${this.key}`;    
+    this.caption = dom.querySelector('h2');
+    this.image = dom.querySelector('img');
+
+    this.onValue = this.itemImages.on('child_added', data => {
+      this.image.src = getUrl(data.val(), 'c_fill,w_500,h_500');
+    });
+
+    this.update(this.item);
+
+    return dom;
+  }
+
+  unrender() {
+    this.itemImages.off('child_added', this.onValue);    
+  }
+}
