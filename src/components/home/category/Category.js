83a173892fa3b2940b68d7b67074605893f62746
Jennifer Elton <justjenny40@hotmail.com>
2018-02-06 10:37:02 -0800

    adding categorie.html

---
+++ src/components/home/category/Category.js
@@ -0,0 +1,16 @@
+import html from './category.html';
+import './category.css';
+import Template from '../Template';
+
+const template = new Template(html);
+
+export default class Category {
+
+  render() {
+    return template.clone();
+  }
+
+  unrender() {
+
+  }
+}
