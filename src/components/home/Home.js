d1f5913acea378d5e33970493b7b7eb8dbed3bf0
Jennifer Elton <justjenny40@hotmail.com>
2018-02-07 13:33:38 -0800

    finished the artist bio on the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,7 @@ import './home.css';
 import Slider from './slider/Slider';
 import Category from './category/Category';
 import Template from '../Template';
+import Featured from './featured/Featured';
 
 const template = new Template(html);
 
@@ -13,7 +14,8 @@ export default class Home {
 
     dom.querySelector('ul').appendChild(new Category().render());
     dom.querySelector('#slider').appendChild(new Slider().render());
-
+    dom.querySelector('ul').appendChild(new Featured().render());
+    
     return dom;
   }
 
